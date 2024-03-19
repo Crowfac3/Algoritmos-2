a3b918a1454facebeeebacc6f55cb554ce904c03
Crowfac3 <83851012+Crowfac3@users.noreply.github.com>
2024-03-18 21:39:04 -0300

    Update TP00-08.py

--- TP00-08.py
+++ TP00-08.py
@@ -36,7 +36,7 @@ sumaTemperatura = 0
 for i in range (1,31):
     while True:
         try:
-            temperatura = round(float(input("Ingrese la temperatura: ")))
+            temperatura = float(round(input("Ingrese la temperatura: ")))
             break
         except ValueError:
             print("Por favor, ingrese una temperatura correcta")
@@ -50,7 +50,7 @@ for i in range (1,31):
     elif temperatura < temperaturaMinima:
         temperaturaMinima = temperatura
 
-print("La temperatura promedio del mes es: ",round((sumaTemperatura/31),2))
-print("La temperatura Maxima registrada fue: ", temperaturaMaxima)
-print("La temperatura Minima registrada fue: ", temperaturaMinima)
+print("La temperatura promedio del mes es: " , round((sumaTemperatura / 31) , 2 ))
+print("La temperatura Maxima registrada fue: " , temperaturaMaxima)
+print("La temperatura Minima registrada fue: " , temperaturaMinima)
 
