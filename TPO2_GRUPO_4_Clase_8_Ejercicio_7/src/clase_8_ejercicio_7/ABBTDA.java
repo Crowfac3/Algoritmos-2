d86f19ddf1d55af24165ec6842868af685c29fcf
Crowfac3 <agarciabohl@hotmail.com>
2024-11-12 17:27:37 -0300

    carpetas

--- TPO2_GRUPO_4_Clase_8_Ejercicio_7/src/clase_8_ejercicio_7/ABBTDA.java
+++
@@ -1,8 +0,0 @@
-package clase_8_ejercicio_7;
-
-public interface ABBTDA<E extends Comparable<E>> {
-	public boolean pertenece( E elemento );
-	public void insertar( E elemento );
-	public E eliminar( E elemento );
-	public String toString();
-}
--- TPO2_GRUPO_4_Clase_8_Ejercicio_7/src/clase_8_ejercicio_7/DefaultComparator.java
+++
@@ -1,6 +0,0 @@
-package clase_8_ejercicio_7;
-
-public class DefaultComparator<E> implements java.util.Comparator<E> { 
-
-	public int compare( E a, E b ) { return((Comparable)a).compareTo(b); } 
-}
